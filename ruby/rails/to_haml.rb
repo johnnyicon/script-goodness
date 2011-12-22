774f8e76f046fd70b681e8022d3adfca1470cd63
John Gonzales <johnnyicon@gmail.com>
2011-12-22 14:16:23 +1300

    Added comments

--- ruby/rails/to_haml.rb
+++ ruby/rails/to_haml.rb
@@ -1,3 +1,8 @@
+# Adapted the script listed in the following link to maintain git history
+# Original Script: http://snippets.dzone.com/posts/show/5449
+# Usage:
+#  1) Copy the file to the root of your Rails app
+#  2) Run the script (i.e., ruby to_haml.rb app/views)
 class ToHaml
   def initialize(path)
     @path = path
